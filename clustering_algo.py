7d25a5a4198fb628b0bfcaf78c9b30ba7a80979e
taylorhawks <taylor.a.hawks@gmail.com>
2020-01-29 16:27:03 -0500

    clustering finished

---
+++ clustering_algo.py
@@ -0,0 +1,80 @@
+class NonlinearModel(LinearModel):
+    def initialize_user_vectors(self,size,embeddings_size):
+
+        starting_vectors = LinearModel.d.get_embedding_vectors(
+            self.item_embeddings,
+            self.user_data,
+            size,
+            embeddings_size=embeddings_size
+        )
+
+        #test out by printing the shape. should be n-users x k x m
+        print(NonlinearModel.Cluster.k_means(starting_vectors,self.T).shape)
+
+
+    class Cluster():
+        @staticmethod
+        def starting_centroids(points,T):
+            #initialize centroids by randomly picking an item for each user
+            starter_idx = np.random.rand(points.shape[0],T).argsort(axis=-1)[:,0:3,np.newaxis]
+            return np.take_along_axis(
+                points,
+                starter_idx,
+                axis=1
+            )
+
+        @staticmethod
+        def closest_centroid(points, centroids):
+            #find the closest centroid to each point
+            return np.argmin(
+                np.square(
+                    np.sum(
+                        np.square(points[:,:,np.newaxis] - centroids[:,np.newaxis]),
+                        axis=3
+                    ),
+                ),
+                axis=2
+            )
+
+        @staticmethod
+        def move_centroids(points,closest,k):
+            #find new centroid based on cluster center from cluster_centroid
+            weights = np.equal(
+                np.arange(k)[np.newaxis,:,np.newaxis],
+                closest[:,np.newaxis,:]
+            )
+
+            weights = np.repeat(weights[:,:,:,np.newaxis],points.shape[2],axis=3)
+
+            points = np.repeat(points[:,np.newaxis],k,axis=1)
+
+            return np.average(
+                points,
+                weights = weights,
+                axis=2
+            )
+
+        @staticmethod
+        def k_means(starting_vectors,k):
+            def iterative_kmeans(starting_vectors,old_centroids, new_centroids,k):
+                while True:
+                    if new_centroids.all() == old_centroids.all():
+                        return new_centroids
+                    else:
+                        old_centroids = new_centroids
+                        new_centroids = NonlinearModel.Cluster.move_centroids(
+                            starting_vectors,
+                            NonlinearModel.Cluster.closest_centroid(
+                                starting_vectors,
+                                old_centroids
+                                ),
+                            k,
+                        )
+                        iterative_kmeans(starting_vectors,old_centroids,new_centroids,k)
+            starting_centroids = NonlinearModel.Cluster.starting_centroids(starting_vectors,k)
+            return iterative_kmeans(
+                starting_vectors,
+                np.zeros(starting_centroids.shape),
+                starting_centroids,
+                k
+            )
